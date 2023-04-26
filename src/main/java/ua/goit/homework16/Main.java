51cc7c7e503c543ccb9ce7d23c9e37219fe3ba00
BabenkoMaksym <Maksym.Babenko.bbn@gmail.com>
2023-04-26 17:26:43 +0200

    test commit 2

--- src/main/java/ua/goit/homework16/Main.java
+++ src/main/java/ua/goit/homework16/Main.java
@@ -3,7 +3,6 @@ package ua.goit.homework16;
 public class Main {
     public static void main(String[] args) {
         GroceryStore groceryStore = new GroceryStore();
-
         Double price = groceryStore.calculateTotalCost(" ABCDABA");
         System.out.println(price);
     }
